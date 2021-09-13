b8833661d7a06f935760b79fb17c124b9f1835ab
sbu_matsini <sibusiso.matsini1@vcontractor.co.za>
2021-09-13 10:20:21 +0200

    bot poc setup - progress

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgxMicrosoftBotFrameworkModule } from 'ngx-microsoft-bot-framework';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +9,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgxMicrosoftBotFrameworkModule
   ],
   providers: [],
   bootstrap: [AppComponent]
